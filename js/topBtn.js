296a3f86edf1677121d41a41d0049c971cc6c6f7
Aleksandra Jemielity <ajemielity@o2.pl>
2017-12-20 13:38:23 +0100

    change hello section, add top btn

---
+++ js/topBtn.js
@@ -0,0 +1,28 @@
+/*// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementsByClassName("top-btn").style.display = "block";
+  } else {
+    document.getElementsByClassName("top-btn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}*/
+
+window.addEventListener('scroll', function() {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop
+    shrinkOn = 300
+    if (distanceY > shrinkOn) {
+      console.log(distanceY)
+      document.getElementById('top-btn').style.display = "block"
+    } else {
+      document.getElementById('top-btn').style.display = "none"
+    }
+  }
+)
